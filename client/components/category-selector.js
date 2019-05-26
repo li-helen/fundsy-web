63f5cc608ddd5046d93f10d95c998e86e18291fa
Helen Li <hca1314@gmail.com>
2019-05-25 23:00:30 -0500

    added categories dropdown in table for each row

--- client/components/category-selector.js
+++ client/components/category-selector.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Button, Dropdown } from 'semantic-ui-react'
+import _ from 'lodash';
+import Dropdown from 'react-toolbox/lib/dropdown';
 
 
 class CategorySelector extends React.Component {
@@ -10,53 +11,47 @@ class CategorySelector extends React.Component {
             categories: [
                 {
                     id: 1,
-                    key: 'rent',
-                    text: 'rent',
-                    value: 'rent'
+                    value: 'rent',
+                    label: 'Rent'
                 },
                 {
                     id: 2,
-                    key: 'utilities',
-                    text: 'utilities',
-                    value: 'utilities'
+                    value: 'utilities',
+                    label: 'Utilities'
                 },
                 {
                     id: 3,
-                    key: 'food',
-                    text: 'food',
-                    value: 'food'
+                    value: 'food',
+                    label: 'Food'
                 },
                 {
                     id: 4,
-                    key: 'transportation',
-                    text: 'transportation',
-                    value: 'transportation'
+                    value: 'transportation',
+                    label: 'Transportation'
                 }
-            ]
+            ],
+
+            selectedCategory: '',
+            dropdownKey: _.uniqueId()
         }
+    }
+
+    handleChange = (value) => {
+        console.log('VALUE CHANGED! VALUE IS NOW: ', value)
+        this.setState({selectedCategory: value})
+        this.setState({dropdownKey: _.uniqueId()})
     }
 
     render() {
         return (
-            <Button.Group color="teal">
-                <Button>Assign a category</Button>
-                <Dropdown 
-                    className='button icon'
-                    floating
-                    options={this.state.categories}
-                    trigger={<React.Fragment />}
-                />
-
-            </Button.Group>
-            // <Dropdown text="Assign a category">
-            //     <Dropdown.Menu> 
-            //     {
-            //         this.state.categories.map(cat => {
-            //             return <Dropdown.Item key={cat.id} text={cat.name} />
-            //         })
-            //     }
-            //     </Dropdown.Menu>
-            // </Dropdown>
+            <Dropdown
+                key={this.state.dropdownKey}
+                label="Assign a category" 
+                auto={false}
+                source={this.state.categories}
+                onChange={this.handleChange}
+                value={this.state.selectedCategory}
+            />
         )
     }
 }
--- client/components/transactions.js
+++ client/components/transactions.js
@@ -1,111 +1,24 @@
-// import React from 'react'
-// import {Table, TableHead, TableRow, TableCell} from 'react-toolbox/lib/table';
+import React, { Component } from 'react';
+import { Table, TableHead, TableRow, TableCell } from 'react-toolbox/lib/table';
+import CategorySelector from './category-selector'
 import { connect } from 'react-redux';
+
 
 // //for most up-to-date docs on tables:
 // //https://github.com/react-toolbox/react-toolbox/tree/dev/components/table#data-table
 
-// class Transactions extends React.Component {
-//     render() {
-//         return (
-//             <Table>
-//                 {/* <TableHead>THIS IS THE TABLE HEAD</TableHead> */}
-//                 <TableRow>
-//                     <TableCell>{data[0].Date}</TableCell>
-//                     <TableCell>{data[0].Account}</TableCell>
-//                     <TableCell>{data[0].Description}</TableCell>
-//                     <TableCell>{data[0].Amount}</TableCell>
-//                     <TableCell>{data[0].Category}</TableCell>
-//                 </TableRow>
-//             </Table>
-//                 // <div>
-//                 // <Table celled>
-//                 //     <Table.Header>
-//                 //         <Table.Row>
-//                 //             <Table.HeaderCell>Date</Table.HeaderCell>
-//                 //             <Table.HeaderCell>Account</Table.HeaderCell>
-//                 //             <Table.HeaderCell>Description</Table.HeaderCell>
-//                 //             <Table.HeaderCell>Amount</Table.HeaderCell>
-//                 //             <Table.HeaderCell>Category</Table.HeaderCell>
-//                 //         </Table.Row>
-//                 //     </Table.Header>
-        
-//                 //     <Table.Body>
-//                 //         {
-//                 //             this.props.transactions.map(transaction => {
-//                 //               return <Table.Row key={transaction.id}>
-//                 //                         <Table.Cell>
-//                 //                             {transaction.date}
-//                 //                         </Table.Cell>
-//                 //                         <Table.Cell>
-//                 //                             {transaction.account}
-//                 //                         </Table.Cell>
-//                 //                         <Table.Cell>
-//                 //                             {transaction.description}
-//                 //                         </Table.Cell>
-//                 //                         <Table.Cell>
-//                 //                             {transaction.amount}
-//                 //                         </Table.Cell>
-//                 //                         <Table.Cell>
-//                 //                             {transaction.category}
-//                 //                         </Table.Cell>
-//                 //                     </Table.Row>
-//                 //             })
-//                 //         }
-//                 //     </Table.Body>
-//                 // </Table>
-//                 // </div>
-//         )
-//     }
-// }
-
-
-
-
-import React, { Component } from 'react';
-import { Table, TableHead, TableRow, TableCell } from 'react-toolbox/lib/table';
-// import Tooltip from '../../components/tooltip';
-
-// const data = [
-//   {name: 'Cupcake', calories: 305, fat: 3.7, sodium: 413, calcium: '3%', iron: '8%'},
-//   {name: 'Donut', calories: 452, fat: 25.0, sodium: 326, calcium: '2%', iron: '22%'},
-//   {name: 'Eclair', calories: 262, fat: 16.0, sodium: 337, calcium: '6%', iron: '7%'},
-//   {name: 'Frozen yogurt', calories: 159, fat: 6.0, sodium: 87, calcium: '14%', iron: '1%'},
-//   {name: 'Gingerbread', calories: 356, fat: 16.0, sodium: 327, calcium: '7%', iron: '16%'},
-//   {name: 'Ice cream sandwich', calories: 237, fat: 9.0, sodium: 129, calcium: '8%', iron: '1%'},
-//   {name: 'Jelly bean', calories: 375, fat: 0.0, sodium: 50, calcium: '0%', iron: '0%'},
-//   {name: 'KitKat', calories: 518, fat: 26.0, sodium: 54, calcium: '12%', iron: '6%'}
-// ];
-
-// const TooltipCell = Tooltip(TableCell);
-
-// const sortByCaloriesAsc = (a, b) => {
-//   if (a.calories < b.calories) return -1;
-//   if (a.calories > b.calories) return 1;
-//   return 0;
-// };
-
-// const sortByCaloriesDesc = (a, b) => {
-//   if (a.calories > b.calories) return -1;
-//   if (a.calories < b.calories) return 1;
-//   return 0;
-// };
-
 class Transactions extends Component {
 
   render () {
-    // const { sorted } = this.state;
-    // const sortedData = this.getSortedData();
     return (
-      <Table multiSelectable style={{ marginTop: 10 }}>
-       {/* table headers */}
-        <TableRow>
+      <Table style={{ marginTop: 10 }} selectable={false}>
+          <TableHead>
             <TableCell>Date</TableCell>
             <TableCell>Account</TableCell>
             <TableCell>Description</TableCell>
             <TableCell>Amount</TableCell>
             <TableCell>Category</TableCell>
-        </TableRow>
+          </TableHead>
 
         {this.props.transactions.map(transaction => {
             return <TableRow key={transaction.id}>
@@ -113,11 +26,9 @@ class Transactions extends Component {
                 <TableCell>{transaction.account}</TableCell>
                 <TableCell>{transaction.description}</TableCell>
                 <TableCell>{transaction.amount}</TableCell>
-                <TableCell>{transaction.category}</TableCell>
+                <TableCell>{<CategorySelector />}</TableCell>
             </TableRow>
         })}
-
-
       </Table>
     );
   }
