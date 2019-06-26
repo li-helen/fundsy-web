30520021c50a996fb335288a10ce44806f35ccee
Helen Li <hca1314@gmail.com>
2019-06-25 20:22:57 -0500

    combined previous routes for retrieving categorized and uncategorized transactions into one

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -4,64 +4,55 @@ const Sequelize = require('sequelize')
 
 const PER_PAGE = 10
 
-router.post('/categorized', async (req, res, next) => {
-    const page = req.query.page
-    try {
-        const transactions = await Transaction.findAll({
-            where: {
-                userId: req.body.userId,
-                categoryId: {
-                    [Sequelize.Op.ne]: null
-                }
-            },
-            limit: 10,
-            offset: page * PER_PAGE,
-            order: [['date', 'DESC']]
-           
-        })
+router.post('/', async (req, res, next) => {
+  const page = req.query.page || 0
+  try {
+    const transactions = await Promise.all([
+      Transaction.findAll({
+        where: {
+          userId: req.body.userId,
+          categoryId: {
+            [Sequelize.Op.ne]: null
+          }
+        },
+        limit: 10,
+        offset: page * PER_PAGE,
+        order: [['date', 'DESC']]
+      }),
 
-        res.send(transactions)
-    } catch(err) {
-        next(err)
-    }
-})
+      Transaction.findAll({
+        where: {
+          userId: req.body.userId,
+          categoryId: null
+        },
+        limit: 10,
+        offset: page * PER_PAGE,
+        order: [['date', 'DESC']]
+      })
+    ])
 
-router.post('/uncategorized', async (req, res, next) => {
-    const page = req.query.page
-    try {
-        const transactions = await Transaction.findAll({
-            where: {
-                userId: req.body.userId,
-                categoryId: null
-            },
-            limit: 10,
-            offset: page * PER_PAGE,
-            order: [['date', 'DESC']]
-        })
-
-        res.send(transactions)
-    } catch(err) {
-        next(err)
-    }
+    res.send(transactions)
+  } catch (err) {
+    next(err)
+  }
 })
 
 router.post('/update-category', async (req, res, next) => {
-    try {
-        const transaction = await Transaction.findOne({
-            where: {
-                id: req.body.transactionId
-            }
-        })
+  try {
+    const transaction = await Transaction.findOne({
+      where: {
+        id: req.body.transactionId
+      }
+    })
 
-        await transaction.update({
-            category: req.body.category
-        })
+    await transaction.update({
+      category: req.body.category
+    })
 
-        res.end()
-
-    } catch(err) {
-        next(err)
-    }
+    res.end()
+  } catch (err) {
+    next(err)
+  }
 })
 
 module.exports = router
