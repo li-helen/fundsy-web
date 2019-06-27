6e72ec1ab31f9ccb461de23eded14fd1e2facdc7
Helen Li <hca1314@gmail.com>
2019-06-27 18:34:11 -0500

    cleaned up POST /transactions route

--- server/api/transactions.js
+++ server/api/transactions.js
@@ -7,7 +7,7 @@ const PER_PAGE = 10
 router.post('/', async (req, res, next) => {
   const page = req.query.page
   try {
-    const transactions = await Promise.all([
+    await Promise.all([
       Transaction.findAll({
         where: {
           userId: req.body.userId,
@@ -32,16 +32,12 @@ router.post('/', async (req, res, next) => {
     ]).then(([categorized, uncategorized]) => {
         res.send(categorized.concat(uncategorized))
     })
-
-    res.send(transactions)
   } catch (err) {
     next(err)
   }
 })
 
 router.put('/update-category', async (req, res, next) => {
-  console.log('transactionId', req.body.transactionId)
-  console.log('CATEGORYID: ', req.body.categoryId)
   try {
     const transaction = await Transaction.findOne({
       where: {
